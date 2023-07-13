4f23b7f6f8f9782fd0b60a669c5f597994b7159f
amb258012@gmail.com <amb258012@gmail.com>
2023-07-13 23:14:33 +0900

    Initial commit

---
+++ src/test/java/jpastudy/japshop/JapshopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpastudy.japshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
