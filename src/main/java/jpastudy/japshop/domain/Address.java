0e8d975f6bec007d5a7f7e482535fe6db2f33df9
amb258012@gmail.com <amb258012@gmail.com>
2023-07-19 00:33:48 +0900

    Address 클래스 수정 - setter 삭제

--- src/main/java/jpastudy/japshop/domain/Address.java
+++ src/main/java/jpastudy/japshop/domain/Address.java
@@ -5,10 +5,19 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String streat;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String streat, String zipcode) {
+        this.city = city;
+        this.streat = streat;
+        this.zipcode = zipcode;
+    }
 }
